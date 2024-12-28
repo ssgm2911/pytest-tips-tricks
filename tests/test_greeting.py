4014f979cc34e45986873c56a56ad2e112bfcfdf
Silvio Guillén M <138510246+ssgm2911@users.noreply.github.com>
2024-12-28 14:22:51 +0000

    adding tests with fixtures

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,4 +1,16 @@
+import pytest
 from greeting import my_name
 
-def test_my_name():
-    assert "My name is: Bob" == my_name("Bob")
+@pytest.fixture
+def bob():
+    return "My name is: Bob"
+
+@pytest.fixture
+def sally():
+    return "My name is: Sally"
+
+def test_bob(bob):
+    assert bob == my_name("Bob")
+
+def test_sally(sally):
+    assert sally == my_name("Sally")
