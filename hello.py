688833c2e3c127452b0203359f0cc3aa144aedf1
Silvio Guillén M <138510246+ssgm2911@users.noreply.github.com>
2024-12-29 03:45:20 +0000

    adding both a library and a web service

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 def more_hello():
-    #import pdb;pdb.set_trace()
+    # import pdb;pdb.set_trace()
     return "hi"
 
 
 def more_goodbye():
     return "bye"
+
 
 if __name__ == "__main__":
     more_hello()
--- web.py
+++ web.py
@@ -1,33 +1,13 @@
 from flask import Flask, jsonify
+from mlib.mchange import change
 
 app = Flask(__name__)
 
-def change(amount):
-    # Calculate the resultant change and store the result(res)
-    res = []
-    coins = [1,5,10,25] # Value of pennies, nickels, dimes, quarters
-    coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
-
-    # Divide the amount*100 (the amount in cents) by a coin value
-    # Record the number of coins that evenly divide and the remainder
-    coin = coins.pop()
-    num, rem = divmod(int(amount*100), coin)
-    # Append the coin type and number of coins that had no remainder
-    res.append({num: coin_lookup[coin]})
-
-    # While there is still some remainder, continue adding coins to the result
-    while rem > 0:
-        coin = coins.pop()
-        num, rem = divmod(rem, coin)
-        if num:
-            if coin in coin_lookup:
-                res.append({num: coin_lookup[coin]})
-    
-    return res
 
 @app.route("/")
 def index():
     return jsonify({"Hello": "Bob"})
+
 
 @app.route("/change/<dollar>/<cents>")
 def changeroute(dollar, cents):
@@ -35,6 +15,7 @@ def changeroute(dollar, cents):
     amount = f"{dollar}.{cents}"
     result = change(float(amount))
     return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
